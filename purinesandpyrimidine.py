4be3a4bb094b6a3b481a89c880b5c137f71a0344
BharathSaravanann <bharathbhc7@gmail.com>
2024-05-09 11:44:10 +0530

    feat:purines and pyrimidines

---
+++ purinesandpyrimidine.py
@@ -0,0 +1,6 @@
+DNA="ATGCGATGACGATGTAGACGATGACGTAG"
+number_of_puries=DNA.count('A')+DNA.count('G')
+number_of_pyrimidines=DNA.count('C')+DNA.count('T')
+print(DNA.find("GACGA"))
+print(number_of_puries)
+print(number_of_pyrimidines)
